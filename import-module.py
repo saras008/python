914a9dce98dbe72192fbc4a1403702120230ae64
iyus <iyus.dedi@detik.com>
2023-05-02 21:19:51 +0700

    day 5 import module

---
+++ import-module.py
@@ -0,0 +1,4 @@
+from module import orang1
+
+for x in orang1.items():
+    print(orang1.keys(), orang1.values())
---
+++ module.py
@@ -0,0 +1,9 @@
+def greating(name):
+    print("Hello my name is",name)
+
+orang1 = {
+    "nama":"iyus",
+    "umur":34,
+    "jenis kelamin":"laki-laki"
+
+}
